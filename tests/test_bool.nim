6324cdf2895ed892f1e23ba5d8bd9a8d32383fe2
Luke Parker <lukeparker5132@gmail.com>
2020-05-11 21:19:32 -0500

    Use check instead of assert.

--- tests/test_bool.nim
+++ tests/test_bool.nim
@@ -17,28 +17,28 @@ type
 
 suite "Test Boolean Encoding/Decoding":
   test "Can encode/decode boolean as signed VarInt":
-    assert not writeValue(PInt(0'i32)).readValue(bool)
-    assert writeValue(PInt(1'i32)).readValue(bool)
-    assert not writeValue(PInt(0'i64)).readValue(bool)
-    assert writeValue(PInt(1'i64)).readValue(bool)
+    check not writeValue(PInt(0'i32)).readValue(bool)
+    check writeValue(PInt(1'i32)).readValue(bool)
+    check not writeValue(PInt(0'i64)).readValue(bool)
+    check writeValue(PInt(1'i64)).readValue(bool)
 
-    assert not writeValue(PIntType(x: 0)).readValue(BoolType).x
-    assert writeValue(PIntType(x: 1)).readValue(BoolType).x
+    check not writeValue(PIntType(x: 0)).readValue(BoolType).x
+    check writeValue(PIntType(x: 1)).readValue(BoolType).x
 
   test "Can encode/decode boolean as unsigned VarInt":
-    assert not writeValue(UInt(0'u32)).readValue(bool)
-    assert writeValue(UInt(1'u32)).readValue(bool)
-    assert not writeValue(UInt(0'u64)).readValue(bool)
-    assert writeValue(UInt(1'u64)).readValue(bool)
+    check not writeValue(UInt(0'u32)).readValue(bool)
+    check writeValue(UInt(1'u32)).readValue(bool)
+    check not writeValue(UInt(0'u64)).readValue(bool)
+    check writeValue(UInt(1'u64)).readValue(bool)
 
-    assert not writeValue(UIntType(x: 0'u)).readValue(BoolType).x
-    assert writeValue(UIntType(x: 1'u)).readValue(BoolType).x
+    check not writeValue(UIntType(x: 0'u)).readValue(BoolType).x
+    check writeValue(UIntType(x: 1'u)).readValue(BoolType).x
 
-  test "Can encode/decode boolean as zigzagged VarInt":
-    assert not writeValue(SInt(0'i32)).readValue(bool)
-    assert writeValue(SInt(1'i32)).readValue(bool)
-    assert not writeValue(SInt(0'i64)).readValue(bool)
-    assert writeValue(SInt(1'i64)).readValue(bool)
+  test "Can encode/decode boolean as zig-zagged VarInt":
+    check not writeValue(SInt(0'i32)).readValue(bool)
+    check writeValue(SInt(1'i32)).readValue(bool)
+    check not writeValue(SInt(0'i64)).readValue(bool)
+    check writeValue(SInt(1'i64)).readValue(bool)
 
-    assert not writeValue(SIntType(x: 0)).readValue(BoolType).x
-    assert writeValue(SIntType(x: 1)).readValue(BoolType).x
+    check not writeValue(SIntType(x: 0)).readValue(BoolType).x
+    check writeValue(SIntType(x: 1)).readValue(BoolType).x
--- tests/test_objects.nim
+++ tests/test_objects.nim
@@ -41,107 +41,107 @@ proc toProtobuf*(value: MyInt): seq[byte] =
 suite "Test Varint Encoding":
   test "Can encode/decode enum field":
     var output = writeValue(SInt(ME3))
-    assert output == @[8.byte, 4]
+    check output == @[8.byte, 4]
 
     let decodedME3 = readValue(output, SInt(MyEnum)).MyEnum
-    assert decodedME3 == ME3
+    check decodedME3 == ME3
 
     output = writeValue(SInt(ME2))
-    assert output == @[8.byte, 2]
+    check output == @[8.byte, 2]
 
     let decodedME2 = readValue(output, SInt(MyEnum)).MyEnum
-    assert decodedME2 == ME2
+    check decodedME2 == ME2
 
   test "Can encode/decode negative number field":
     let num = -153452
 
     var output = writeValue(SInt(num))
-    assert output == @[8.byte, 215, 221, 18]
+    check output == @[8.byte, 215, 221, 18]
 
     let decoded = readValue(output, SInt(int)).int
-    assert decoded == num
+    check decoded == num
 
   test "Can encode/decode distinct number field":
     let num = 114151.MyInt
-    assert(num.int == num.toProtobuf().fromProtobuf[:MyInt]().int)
+    check(num.int == num.toProtobuf().fromProtobuf[:MyInt]().int)
 
     var output = writeValue(num)
-    assert output == @[10.byte, 3, 231, 189, 1]
+    check output == @[10.byte, 3, 231, 189, 1]
 
     let decoded = readValue(output, MyInt)
-    assert decoded.int == num.int
+    check decoded.int == num.int
 
   test "Can encode/decode float32 number field":
     let num = float32(1234.164423)
 
     var output = writeValue(num)
-    assert output == @[13.byte, 67, 69, 154, 68]
+    check output == @[13.byte, 67, 69, 154, 68]
 
     let decoded = readValue(output, float32)
-    assert decoded == num
+    check decoded == num
 
   test "Can encode/decode float64 number field":
     let num = 12343121537452.1644232341'f64
 
     var output = writeValue(num)
-    assert output == @[9.byte, 84, 88, 211, 191, 182, 115, 166, 66]
+    check output == @[9.byte, 84, 88, 211, 191, 182, 115, 166, 66]
 
     let decoded = readValue(output, float64)
-    assert decoded == num
+    check decoded == num
 
   test "Can encode/decode bool field":
     let boolean = true
 
     var output = writeValue(UInt(boolean))
-    assert output == @[8.byte, 1]
+    check output == @[8.byte, 1]
 
     let decoded = bool(readValue(output, UInt(bool)))
-    assert decoded == boolean
+    check decoded == boolean
 
   test "Can encode/decode char field":
     let charVal = 'G'
 
     var output = writeValue(UInt(charVal))
-    assert output == @[8.byte, ord(charVal).byte]
+    check output == @[8.byte, ord(charVal).byte]
 
     let decoded = readValue(output, UInt(char)).char
-    assert decoded == charVal
+    check decoded == charVal
 
   test "Can encode/decode unsigned number field":
     let num = 123151.uint
 
     var output = writeValue(UInt(num))
-    assert output == @[8.byte, 143, 194, 7]
+    check output == @[8.byte, 143, 194, 7]
 
     let decoded = readValue(output, UInt(uint)).uint
-    assert decoded == num
+    check decoded == num
 
   test "Can encode/decode string field":
     let str = "hey this is a string"
 
     var output = writeValue(str)
-    assert output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
+    check output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
 
     let decoded = readValue(output, string)
-    assert decoded == str
+    check decoded == str
 
   test "Can encode/decode char seq field":
     let charSeq = cast[seq[char]]("hey this is a string")
 
     var output = writeValue(charSeq)
-    assert output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
+    check output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
 
     let decoded = readValue(output, seq[char])
-    assert decoded == charSeq
+    check decoded == charSeq
 
   test "Can encode/decode uint8 seq field":
     let uint8Seq = cast[seq[uint8]]("hey this is a string")
 
     var output = writeValue(uint8Seq)
-    assert output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
+    check output == @[10.byte, 20, 104, 101, 121, 32, 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 116, 114, 105, 110, 103]
 
     let decoded = readValue(output, seq[uint8])
-    assert decoded == uint8Seq
+    check decoded == uint8Seq
 
   test "Can encode/decode object":
 
@@ -149,7 +149,7 @@ suite "Test Varint Encoding":
 
     var output = writeValue(obj)
     let decoded = output.readValue(Test3)
-    assert decoded == obj
+    check decoded == obj
 
   test "Can encode/decode out of order object":
     let obj = Test3(g: 400, h: 100, i: Test1(a: 100, b: "this is a test", c: 'H'), j: "testing", k: true, l: 14514.MyInt)
@@ -165,22 +165,22 @@ suite "Test Varint Encoding":
     var output = writer.buffer()
     let decoded = output.readValue(Test3)
 
-    assert decoded == obj
+    check decoded == obj
 
   test "Empty object field does not get encoded":
     let obj = Test1()
 
     var output = writeValue(obj)
-    assert output.len == 0
+    check output.len == 0
 
     let decoded = output.readValue(Test1)
-    assert decoded == obj
+    check decoded == obj
 
   test "Empty object does not get encoded":
     let obj = Test1()
 
     var output = writeValue(obj)
-    assert output.len == 0
+    check output.len == 0
 
     let decoded = output.readValue(Test1)
-    assert decoded == obj
+    check decoded == obj
