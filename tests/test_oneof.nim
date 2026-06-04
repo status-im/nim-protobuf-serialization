ad6fc256ce05cc297842b07640fec07d90e1beda
nitely <ecastroborsani@gmail.com>
2026-06-04 08:00:12 -0300

    wip

--- tests/test_oneof.nim
+++ tests/test_oneof.nim
@@ -193,3 +193,105 @@ suite "Test many oneof":
       ret.one.y == 1
       ret.two.kind == Kind1.y
       ret.two.y == 1
+
+type
+  KindAll {.pure.} = enum
+    unset
+    x01
+    x02
+    x03
+    x04
+    x05
+    x06
+    x07
+    x08
+    x09
+    x10
+    x11
+    x12
+    x13
+    x14
+
+  OneOfAll {.proto3, oneof.} = object
+    case kind: KindAll
+    of KindAll.unset:
+      discard
+    of KindAll.x01:
+      x01 {.fieldNumber: 1.}: string
+    of KindAll.x02:
+      x02 {.fieldNumber: 2.}: seq[byte]
+    of KindAll.x03:
+      x03 {.fieldNumber: 3, pint.}: int32
+    of KindAll.x04:
+      x04 {.fieldNumber: 4, pint.}: uint32
+    of KindAll.x05:
+      x05 {.fieldNumber: 5, pint.}: int64
+    of KindAll.x06:
+      x06 {.fieldNumber: 6, pint.}: uint64
+    of KindAll.x07:
+      x07 {.fieldNumber: 7, sint.}: int32
+    of KindAll.x08:
+      x08 {.fieldNumber: 8, sint.}: int64
+    of KindAll.x09:
+      x09 {.fieldNumber: 9, fixed.}: int32
+    of KindAll.x10:
+      x10 {.fieldNumber: 10, fixed.}: int64
+    of KindAll.x11:
+      x11 {.fieldNumber: 11, fixed.}: uint32
+    of KindAll.x12:
+      x12 {.fieldNumber: 12, fixed.}: uint64
+    of KindAll.x13:
+      x13 {.fieldNumber: 13.}: float32
+    of KindAll.x14:
+      x14 {.fieldNumber: 14.}: float64
+
+  ObjAll {.proto3.} = object
+    one {.oneof.}: OneOfAll
+
+suite "Test all types in oneof":
+  test "all oneof unset":
+    let encoded = "".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.unset
+      Protobuf.encode(ret) == encoded
+
+  test "string oneof set":
+    let encoded = "0a03616263".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.x01
+      ret.one.x01 == "abc"
+      Protobuf.encode(ret) == encoded
+
+  test "bytes oneof set":
+    let encoded = "120101".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.x02
+      ret.one.x02 == @[0x01'u8]
+      Protobuf.encode(ret) == encoded
+
+  test "pint int32 oneof set":
+    let encoded = "187b".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.x03
+      ret.one.x03 == 123
+      Protobuf.encode(ret) == encoded
+
+  test "sint int32 oneof set":
+    let encoded = "38f601".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.x07
+      ret.one.x07 == 123
+      Protobuf.encode(ret) == encoded
+
+  test "fixed int32 oneof set":
+    let encoded = "4d7b000000".hexToSeqByte
+    let ret = Protobuf.decode(encoded, ObjAll)
+    check:
+      ret.one.kind == KindAll.x09
+      ret.one.x09 == 123
+      Protobuf.encode(ret) == encoded
