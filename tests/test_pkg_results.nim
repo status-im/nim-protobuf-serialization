eb777c9c0e914d2f845325f37f8ed34f01e211d3
nitely <ecastroborsani@gmail.com>
2026-06-05 15:30:44 -0300

    Support `results.Opt` in `proto3`

--- tests/test_pkg_results.nim
+++ tests/test_pkg_results.nim
@@ -23,14 +23,61 @@ type
     a {.fieldNumber: 3.}: Opt[string]
     b {.fieldNumber: 4.}: Opt[FewOptions]
 
+  P3FewOptions {.proto3.} = object
+    a {.fieldNumber: 1, pint.}: Opt[int32]
+    b {.fieldNumber: 2, pint.}: Opt[int32]
+
+  P3FullOfDefaults {.proto3.} = object
+    a {.fieldNumber: 3.}: Opt[string]
+    b {.fieldNumber: 4.}: Opt[P3FewOptions]
+
 suite "Test results Opt[T]":
-  test "sets optional valid field":
+  test "proto2 sets optional valid field":
     # echo 'b: { b: 5 }' | protoc --encode=FullOfDefaults test_protobuf2_semantics.proto | hexdump -ve '1/1 "%.2x"'
     # 22021005
     roundtrip(FullOfDefaults(b: Opt.some(FewOptions(b: Opt.some(5'i32)))), "22021005")
 
-  test "unexpected type does not set Opt":
-    # echo "0801" | xxd -r -p | protoc --decode=FixedOption test_protobuf2_semantics.proto
+  test "proto2 unexpected type does not set Opt":
+    # echo "0801" | xxd -r -p | protoc --decode=FullOfDefaults test_protobuf2_semantics.proto
     # 1: 1
     let encoded = "0801".hexToSeqByte
     check Protobuf.decode(encoded, FullOfDefaults) == FullOfDefaults()
+
+  test "proto2 optional default":
+    # echo 'a: 0' | protoc --encode=FewOptions test_pkg_results_2.proto | hexdump -ve '1/1 "%.2x"'
+    # 0800
+    # echo "0800" | xxd -r -p | protoc --decode=FewOptions test_pkg_results_2.proto
+    # a: 0
+    roundtrip(FewOptions(a: Opt.some(0'i32)), "0800")
+
+  test "proto2 optional set":
+    # echo 'a: 1' | protoc --encode=FewOptions test_pkg_results_2.proto | hexdump -ve '1/1 "%.2x"'
+    # 0801
+    # echo "0801" | xxd -r -p | protoc --decode=FewOptions test_pkg_results_2.proto
+    # a: 1
+    roundtrip(FewOptions(a: Opt.some(1'i32)), "0801")
+
+  test "proto3 sets optional valid field":
+    # echo 'b: { b: 5 }' | protoc --encode=P3FullOfDefaults test_pkg_results_3.proto | hexdump -ve '1/1 "%.2x"'
+    # 22021005
+    roundtrip(P3FullOfDefaults(b: Opt.some(P3FewOptions(b: Opt.some(5'i32)))), "22021005")
+
+  test "proto3 unexpected type does not set Opt":
+    # echo "0801" | xxd -r -p | protoc --decode=P3FullOfDefaults test_pkg_results_3.proto
+    # 1: 1
+    let encoded = "0801".hexToSeqByte
+    check Protobuf.decode(encoded, P3FullOfDefaults) == P3FullOfDefaults()
+
+  test "proto3 optional default":
+    # echo 'a: 0' | protoc --encode=P3FewOptions test_pkg_results_3.proto | hexdump -ve '1/1 "%.2x"'
+    # 0800
+    # echo "0800" | xxd -r -p | protoc --decode=P3FewOptions test_pkg_results_3.proto
+    # a: 0
+    roundtrip(P3FewOptions(a: Opt.some(0'i32)), "0800")
+
+  test "proto3 optional set":
+    # echo 'a: 1' | protoc --encode=P3FewOptions test_pkg_results_3.proto | hexdump -ve '1/1 "%.2x"'
+    # 0801
+    # echo "0801" | xxd -r -p | protoc --decode=P3FewOptions test_pkg_results_3.proto
+    # a: 1
+    roundtrip(P3FewOptions(a: Opt.some(1'i32)), "0801")
